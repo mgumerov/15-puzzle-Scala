09c871ad39ab0014a96034c3c1f3405ecd9e0832
mgumerov <maxim.gumerov@massmo.ru>
2015-01-21 10:40:31 +0500

    The basic idea

---
+++ Puzzle.scala
@@ -0,0 +1,19 @@
+object Puzzle {
+  @scala.annotation.tailrec
+  def step(board: Array[Int]) {
+    println(board.mkString(","));
+    val ln = scala.io.StdIn.readLine();
+    if (ln == "") return;
+    val target = ln.toInt;
+    val newboard = board.map(x => x match {
+        case `target` => 0;
+        case 0 => target;
+        case _ => x;
+      });
+    step(newboard);
+  }
+
+  def main(args: Array[String]) {
+    step((0 to 15).toArray);
+  }
+}
